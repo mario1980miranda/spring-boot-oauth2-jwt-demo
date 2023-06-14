93a8884c3f86af58c6920af94a545328759def83
Mario Miranda <mario_chan15@hotmail.com>
2023-06-14 09:36:17 -0400

    Role, Mapeamento JPA e seed

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/User.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/User.java
@@ -1,82 +1,104 @@
 package com.devsuperior.demo.entities;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
 
-    @Column(unique = true)
-    private String email;
-    private String password;
+	@Column(unique = true)
+	private String email;
+	private String password;
 
-    public User() {
-    }
+	@ManyToMany
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 
-    public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
+	public User() {
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void addRole(Role role) {
+		this.roles.add(role);
+	}
+	
+	public boolean hasRole(final String roleName) {
+		for (Role role : roles)
+			if (role.getAuthority().equals(roleName))
+				return true;
+		return false;
+	}
 
-        User user = (User) o;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
 
-        return Objects.equals(id, user.id);
-    }
+		User user = (User) o;
 
-    @Override
-    public int hashCode() {
-        return id != null ? id.hashCode() : 0;
-    }
+		return Objects.equals(id, user.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return id != null ? id.hashCode() : 0;
+	}
 }
