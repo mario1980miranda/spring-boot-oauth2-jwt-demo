a7366022e7f9a099f89d44ca8c2e88d8a84d56ed
Mario Miranda <mario_chan15@hotmail.com>
2023-06-14 14:52:00 -0400

    FIX native query

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/repositories/UserRepository.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/repositories/UserRepository.java
@@ -11,8 +11,8 @@ import com.devsuperior.demo.projections.UserDetailsProjection;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	@Query(nativeQuery = true, value = 
-			  "SELECT tb_user.email AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority"
-			+ "FROM tb_user" 
+			  "SELECT tb_user.email AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority "
+			+ "FROM tb_user " 
 			+ "INNER JOIN tb_user_role ON tb_user.id = tb_user_role.user_id "
 			+ "INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id "
 			+ "WHERE tb_user.email = :email")
